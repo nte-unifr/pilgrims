abcb4f7e686c51278e7fc12b2bb54e037019fc95
Alrick <alrick.deillon@unifr.ch>
2016-06-01 10:17:00 +0200

    add popup for images

--- web/js/pilgrims.js
+++ web/js/pilgrims.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-    // init Masonry
-    var $grid = $('.grid').masonry({
-        itemSelector: '.grid-item'
-    });
-    // layout Masonry after each image loads
-    $grid.imagesLoaded().progress( function() {
-        $grid.masonry('layout');
-    });
+
+    // Masonry
+    if ( $( ".grid" ).length ) {
+        // init Masonry
+        var $grid = $('.grid').masonry({
+            itemSelector: '.grid-item'
+        });
+        // layout Masonry after each image loads
+        $grid.imagesLoaded().progress( function() {
+            $grid.masonry('layout');
+        });
+    }
+
+    // Magnific popup
+    $('.image-popup').magnificPopup({type:'image'});
 });
