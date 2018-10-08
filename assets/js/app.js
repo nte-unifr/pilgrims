00494f3522d9cca12da94504362a98940912b9a6
Alrick <alrick.deillon@unifr.ch>
2018-10-08 17:20:10 +0200

    add map to site page

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,16 @@ require('../css/app.scss')
 var $ = require('jquery')
 import mixitup from 'mixitup'
 require('bootstrap')
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
+mapboxgl.accessToken = 'pk.eyJ1IjoibnRlIiwiYSI6IjNuaWc4XzQifQ.FV_ZIkwWG_gJKP7PIdLuJw'
 
 $(document).ready(function () {
   initBsElements()
-  if ($( "#sites" ).length) {
+  if ($('#sites').length) {
     initMixitup()
+  }
+  if ($('#site').length) {
+    drawMap()
   }
 })
 
@@ -35,4 +40,22 @@ function initMixitup() {
       }
     }
   })
+}
+
+function drawMap() {
+  var container = $('#map')
+  var lat = container.data('lat')
+  var lng = container.data('lng')
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/basic-v9',
+    zoom: 9,
+    center: [lng, lat]
+  })
+
+  var ll = new mapboxgl.LngLat(lng, lat)
+  new mapboxgl.Marker().setLngLat(ll).addTo(map)
+
+  // we need to resize the map when the tab is shown because it doesn't have any height before
+  $('#map-tab').on('shown.bs.tab', function (e) { map.resize() })
 }
