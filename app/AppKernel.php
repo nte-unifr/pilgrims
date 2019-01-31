b30109f69ff3e0fe6fb6a37e92b6f05b725d887c
Alrick <alrick.deillon@unifr.ch>
2019-01-31 16:41:57 +0100

    generator and ws bundles exception for sf34

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,8 +27,11 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            }
         }
 
         return $bundles;
